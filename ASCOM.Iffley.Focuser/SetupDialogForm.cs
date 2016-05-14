b42d388d5a237f5133218ba73e0fa7e7c85d45ee
Themos Tsikas <themos.tsikas@gmail.com>
2016-05-14 12:59:29 +0100

    After creation by ASCOM template and Tim Long's SettingsProvider edits (see youtube video)

---
+++ ASCOM.Iffley.Focuser/SetupDialogForm.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using ASCOM.Utilities;
+using ASCOM.Iffley;
+
+namespace ASCOM.Iffley
+{
+    [ComVisible(false)]					// Form not registered for COM!
+    public partial class SetupDialogForm : Form
+    {
+        public SetupDialogForm()
+        {
+            InitializeComponent();
+            // Initialise current values of user settings from the ASCOM Profile
+            //CMNT2 InitUI();
+        }
+
+        private void cmdOK_Click(object sender, EventArgs e) // OK button event handler
+        {
+            // Place any validation constraint checks here
+            // Update the state variables with results from the dialogue
+            //CMNT1 Focuser.comPort = (string)comboBoxComPort.SelectedItem;
+            //CMNT1 Focuser.tl.Enabled = chkTrace.Checked;
+        }
+
+        private void cmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
+        {
+            Close();
+        }
+
+        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("http://ascom-standards.org/");
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+
+        private void comboBoxComPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
